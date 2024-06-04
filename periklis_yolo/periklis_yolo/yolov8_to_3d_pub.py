8186279c31d2b8328ca5dc3a9f81ba43efb52865
FranciscoMColino <francisco.m.colino@gmail.com>
2024-06-04 16:32:39 +0100

    Add node publishing detection bboxes

--- periklis_yolo/periklis_yolo/yolov8_to_3d_pub.py
+++ periklis_yolo/periklis_yolo/yolov8_to_3d_pub.py
@@ -40,30 +40,163 @@ class ImageDepthSyncVizSubscriber(Node):
 
         self.ts = ApproximateTimeSynchronizer([self.image_sub, self.camera_info_sub, self.depth_sub], queue_size=10, slop=0.1)
         self.ts.registerCallback(self.callback)
+
+        self.model = YOLO('yolov8n.engine')
+
+    def compute_points_from_bbox(self, bbox, resize_factor, fx, fy, cx, cy, depth_image):
+        u1, v1, u2, v2 = bbox // resize_factor
+
+        # check v1, v2, u1, u2 bounds
+        v1 = max(0, v1)
+        v1 = min(depth_image.shape[0] - 1, v1)
+        v2 = max(0, v2)
+        v2 = min(depth_image.shape[0] - 1, v2)
+        u1 = max(0, u1)
+        u1 = min(depth_image.shape[1] - 1, u1)
+        u2 = max(0, u2)
+        u2 = min(depth_image.shape[1] - 1, u2)
+
+        depth_values_bb = depth_image[v1:v2, u1:u2]
+        depth_values_bb = depth_values_bb[np.isfinite(depth_values_bb)]
+        if len(depth_values_bb) == 0:
+            print('No finite depth values in the bounding box')
+            return None
+        min_depth = np.min(depth_values_bb)
+
+        percentile = 15
+        percentile_depth_values = np.percentile(depth_values_bb, percentile)
+
+        z1 =  percentile_depth_values * 1.15
+
+        if not np.isfinite(z1):
+            # use median depth value around the point
+            median_filter_radius = 3
+            z1 = np.nanmedian(depth_image[max(0, v1 - median_filter_radius):min(depth_image.shape[0] - 1, v1 + median_filter_radius), max(0, u1 - median_filter_radius):min(depth_image.shape[1] - 1, u1 + median_filter_radius)])
+
+
+        x1 = ((u1 - cx) * z1) / fx
+        y1 = ((v1 - cy) * z1) / fy
+
+        x1, y1, z1 = z1, -x1, -y1
+
+        z2 = min_depth
+
+        if not np.isfinite(z2):
+            # use median depth value around the point
+            median_filter_radius = 3
+            z2 = np.nanmedian(depth_image[max(0, v2 - median_filter_radius):min(depth_image.shape[0] - 1, v2 + median_filter_radius), max(0, u2 - median_filter_radius):min(depth_image.shape[1] - 1, u2 + median_filter_radius)])
+
+        x2 = ((u2 - cx) * z2) / fx
+        y2 = ((v2 - cy) * z2) / fy
+
+        x2, y2, z2 = z2, -x2, -y2
+
+        points = np.array([
+                    [x1, y1, z1],
+                    [x1, y1, z2],
+                    [x1, y2, z1],
+                    [x1, y2, z2],
+                    [x2, y1, z1],
+                    [x2, y1, z2],
+                    [x2, y2, z1],
+                    [x2, y2, z2]
+                ])
+
+        return points
     
-    # http://docs.ros.org/en/noetic/api/sensor_msgs/html/msg/CameraInfo.html
+    def build_ember_bbox(self, points):
+        ember_bbox = EmberBoundingBox3D()
+        if points.shape != (8, 3):
+            return ember_bbox
+        ember_bbox.corner1 = Point(x=points[0, 0], y=points[0, 1], z=points[0, 2])
+        ember_bbox.corner2 = Point(x=points[1, 0], y=points[1, 1], z=points[1, 2])
+        ember_bbox.corner3 = Point(x=points[2, 0], y=points[2, 1], z=points[2, 2])
+        ember_bbox.corner4 = Point(x=points[3, 0], y=points[3, 1], z=points[3, 2])
+        ember_bbox.corner5 = Point(x=points[4, 0], y=points[4, 1], z=points[4, 2])
+        ember_bbox.corner6 = Point(x=points[5, 0], y=points[5, 1], z=points[5, 2])
+        ember_bbox.corner7 = Point(x=points[6, 0], y=points[6, 1], z=points[6, 2])
+        ember_bbox.corner8 = Point(x=points[7, 0], y=points[7, 1], z=points[7, 2])
+        return ember_bbox
+
+    def draw_cv2_bounding_box(self, box, bbox, bgr_image):
+        u1, v1, u2, v2 = bbox
+        confidence = box.conf[0].item()
+        class_id = int(box.cls[0].item())
+        label = f'{self.model.names[class_id]}: {confidence:.2f}'
+        color = (0, 255, 0)
+        cv2.rectangle(bgr_image, (u1, v1), (u2, v2), color, 2)
+        cv2.putText(bgr_image, label, (u1, v1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
+        
+
     def callback(self, msg_image, msg_camera_info, msg_depth):
         # log timestamp
         self.get_logger().info(f"ImageDepthSyncVizSubscriber callback: {msg_image.header.stamp}")
 
-        # create mock ember bounding box
-        ember_bbox = EmberBoundingBox3D()
-        ember_bbox.corner1 = Point(x=0.0, y=0.0, z=0.0)
-        ember_bbox.corner2 = Point(x=1.0, y=0.0, z=0.0)
-        ember_bbox.corner3 = Point(x=1.0, y=1.0, z=0.0)
-        ember_bbox.corner4 = Point(x=0.0, y=1.0, z=0.0)
-        ember_bbox.corner5 = Point(x=0.0, y=0.0, z=1.0)
-        ember_bbox.corner6 = Point(x=1.0, y=0.0, z=1.0)
-        ember_bbox.corner7 = Point(x=1.0, y=1.0, z=1.0)
-        ember_bbox.corner8 = Point(x=0.0, y=1.0, z=1.0)
-        
-        ember_bbox_array = EmberBoundingBox3DArray()
-        ember_bbox_array.header = Header()
-        ember_bbox_array.header.stamp = msg_image.header.stamp
-        ember_bbox_array.header.frame_id = "ember_bbox_frame"
-        ember_bbox_array.boxes.append(ember_bbox)
+        fx, fy = msg_camera_info.k[0], msg_camera_info.k[4]
+        cx, cy = msg_camera_info.k[2], msg_camera_info.k[5]
 
+        bgr_image = CvBridge().imgmsg_to_cv2(msg_image, desired_encoding='bgr8')
+        depth_image = CvBridge().imgmsg_to_cv2(msg_depth, desired_encoding='passthrough')
+
+        # TODO read this settings from config file
+        bgr_detection_resize_factor = 4
+        zed_ros2_wrapper_downscale_factor = 8
+        bgr_resized_to_depth_ratio = 2
+
+        fx /= zed_ros2_wrapper_downscale_factor
+        fy /= zed_ros2_wrapper_downscale_factor
+        cx /= zed_ros2_wrapper_downscale_factor
+        cy /= zed_ros2_wrapper_downscale_factor
+
+        bgr_resized = cv2.resize(bgr_image, (bgr_image.shape[1] // bgr_detection_resize_factor, bgr_image.shape[0] // bgr_detection_resize_factor))
+
+        ember_bbox_array = EmberBoundingBox3DArray()
+
+        results = self.model(bgr_resized)[0]
+
+        for result in results:
+            boxes = result.boxes
+            # masks = result.masks TODO
+            for i in range(len(boxes)):
+
+                # if class is not person, skip
+                if boxes[i].cls[0].item() != 0:
+                    continue
+
+                u1, v1, u2, v2 = map(int, boxes[i].xyxy[0].tolist())
+
+                points = self.compute_points_from_bbox(np.array([u1, v1, u2, v2]), bgr_resized_to_depth_ratio, fx, fy, cx, cy, depth_image)
+
+                ember_bbox = self.build_ember_bbox(points)
+                ember_bbox_array.boxes.append(ember_bbox)
+
+                self.draw_cv2_bounding_box(boxes[i], (u1, v1, u2, v2), bgr_resized)
+        
         self.bbox_pub.publish(ember_bbox_array)
+  
+        cv2.imshow('YOLOv8 Detection', bgr_resized)
+        cv2.waitKey(1)
+
+
+
+        # create mock ember bounding box
+        # ember_bbox = EmberBoundingBox3D()
+        # ember_bbox.corner1 = Point(x=0.0, y=0.0, z=0.0)
+        # ember_bbox.corner2 = Point(x=1.0, y=0.0, z=0.0)
+        # ember_bbox.corner3 = Point(x=1.0, y=1.0, z=0.0)
+        # ember_bbox.corner4 = Point(x=0.0, y=1.0, z=0.0)
+        # ember_bbox.corner5 = Point(x=0.0, y=0.0, z=1.0)
+        # ember_bbox.corner6 = Point(x=1.0, y=0.0, z=1.0)
+        # ember_bbox.corner7 = Point(x=1.0, y=1.0, z=1.0)
+        # ember_bbox.corner8 = Point(x=0.0, y=1.0, z=1.0)
+        # 
+        # ember_bbox_array = EmberBoundingBox3DArray()
+        # ember_bbox_array.header = Header()
+        # ember_bbox_array.header.stamp = msg_image.header.stamp
+        # ember_bbox_array.header.frame_id = "ember_bbox_frame"
+        # ember_bbox_array.boxes.append(ember_bbox)
+        #     
+        # self.bbox_pub.publish(ember_bbox_array)
 
 
 def main(args=None):
