7a782dc717ad58ab08e16fb3903141fe2cdcd5e2
FranciscoMColino <francisco.m.colino@gmail.com>
2024-06-05 16:33:28 +0100

    Add template for yolo 3d bbox prediction using masks

--- periklis_yolo/setup.py
+++ periklis_yolo/setup.py
@@ -30,6 +30,7 @@ setup(
             'yolov8_cv2_viz= periklis_yolo.yolov8_cv2_viz:main',
             'yolov8_to_3d_viz= periklis_yolo.yolov8_to_3d_viz:main',
             'yolov8_to_3d_pub= periklis_yolo.yolov8_to_3d_pub:main',
+            'yolo_mask_3d_viz= periklis_yolo.yolo_mask_3d_viz:main',
         ],
     },
 )
