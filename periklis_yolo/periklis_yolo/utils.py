Fixed bbox coordinates calculation

periklis_yolo/periklis_yolo/utils.py
@@ -286,14 +286,14 @@ def compute_3d_bbox_from_parents_2(parent_point_1, parent_point_2):
     dist_parents = np.linalg.norm(parent_point_1 - parent_point_2)
 
     points = np.array([
-        [center[0] + dist_parents/2, center[1], z1,],
-        [center[0] - dist_parents/2, center[1], z1,],
-        [center[0] + dist_parents/2, center[1], z2,],
-        [center[0] - dist_parents/2, center[1], z2,],
-        [center[0], center[1] + dist_parents/2, z1,],
-        [center[0], center[1] - dist_parents/2, z1,],
-        [center[0], center[1] + dist_parents/2, z2,],
-        [center[0], center[1] - dist_parents/2, z2]
+        [center[0] + dist_parents/2, center[1] + dist_parents/2, z1],
+        [center[0] - dist_parents/2, center[1] + dist_parents/2, z1],
+        [center[0] + dist_parents/2, center[1] - dist_parents/2, z1],
+        [center[0] - dist_parents/2, center[1] - dist_parents/2, z1],
+        [center[0] + dist_parents/2, center[1] + dist_parents/2, z2],
+        [center[0] - dist_parents/2, center[1] + dist_parents/2, z2],
+        [center[0] + dist_parents/2, center[1] - dist_parents/2, z2],
+        [center[0] - dist_parents/2, center[1] - dist_parents/2, z2]
     ])
 
     return points